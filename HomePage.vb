76ac2fb102cf2b2efb3931c5860cc895785c66a6
Daniela Airies Maravilla <36101806+iamseiria@users.noreply.github.com>
2018-03-18 23:49:10 +0800

    Home Page

---
+++ HomePage.vb
@@ -0,0 +1,30 @@
+﻿Public Class HomePage
+
+    Private Sub btnLogOut_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLogOut.Click
+        btnLogOut.Show()
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnRegEmployee_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRegEmployee.Click
+        Dim RegisterEmployee As New Userfrm
+        RegisterEmployee.Show()
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnListOfEmployee_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnListOfEmployee.Click
+        Dim employees As New ListOfEmployees
+        employees.Show()
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnAttendanceSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAttendanceSummary.Click
+        Dim attendance As New AttendanceSummary
+        attendance.Show()
+        Me.Close()
+
+
+    End Sub
+End Class
