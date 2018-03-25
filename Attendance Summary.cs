0700f4a013cb009ced54db53fb97e9525d9bb4d0
Judylene Gabia <37566635+judylene@users.noreply.github.com>
2018-03-25 10:03:49 +0800

    Add files via upload

---
+++ Attendance Summary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Employee_Form
+{
+    public partial class Attendance_Summary : Form
+    {
+        public Attendance_Summary()
+        {
+            InitializeComponent();
+        }
+
+        private void btnahome_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Home_Page hpage = new Home_Page();
+            hpage.ShowDialog();
+        }
+    }
+}
